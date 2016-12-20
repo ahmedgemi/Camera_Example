ed64be92a7f9ad99633007c36e16170b5f95bb3a
codeghost94 <ahmedgamal.cufe@gmail.com>
2016-12-20 10:52:20 +0200

    commit

---
+++ app/src/main/java/com/example/ahmed/camera_example/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.ahmed.camera_example;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView iv ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void openCamera(View v){
+
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+
+        startActivityForResult(intent,111);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        iv.setImageBitmap(bp);
+    }
+}
